2c6685427126536c90541329bf1206fe218c20ef
Roxybli <lizama.roxana@hotmail.com>
2018-10-18 20:45:51 -0600

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
-$config['base_url'] = 'http://localhost/CM/sistemaTRv4/';
-=======
-$config['base_url'] = 'http://localhost/TrabajoRox/sistemaTRv4/';
->>>>>>> pr/1
+$config['base_url'] = 'http://localhost/sistemaTRv4/';
 
 /*
 |--------------------------------------------------------------------------
